109db302ce943da537877367299cd52ef01d2e22
hamazaki1990 <midsummerbeach@yahoo.co.jp>
2019-01-11 17:39:38 +0900

    kmer

--- check_kmers_test.py
+++ check_kmers_test.py
@@ -16,18 +16,31 @@ cent.remove("5")
 cent.remove("19")
 
 
-input_q = "test1.fa"
-input_s = "test2.fa"
+for k in range(15, 36):
+    for i in chr:
+        print("chr" + str(i))
+        input_s = "chr" + i + "_repeatmasked.fa"
+        try:
+            genome = next(SeqIO.parse(input_s, "fasta"))
+        except:
+            break
+        else:
+            for j in cent:
+                print(str(j))
+                input_q = "chr" + j + "_HOR.fa"
+                output = "chr" + i + "_" + str(k) + "mer_chr" + str(j) + "HOR_hit_test.csv"
+                kmer = collect_kmers(input_q, "fasta", k)
 
-k = 3
-
-kmer = collect_kmers(input_q, "fasta", k)
-
-
-for i in kmer:
-    for seq_record in SeqIO.parse(input_s, "fasta"):
-        index = seq_record.seq.find(i)
-        split = seq_record.seq.split(i)
-        length = [len(j) for j in split]
-        position = [sum(length[:(l + 1)]) + l * k for l in range(len(length) - 1)]
-        print(i, split, length, index, position)
+                with open(output, "w") as outfile:
+                    for l in kmer:
+                        index = genome.seq.find(l)
+                        if index == -1:
+                            pass
+                        else:
+                            split = genome.seq.split(l)
+                            length = [len(x) for x in split]
+                            position = [sum(length[:(x + 1)]) + x * k for x in range(len(length) - 1)]
+                            row = [l]
+                            row.extend(position)
+                            writer = csv.writer(outfile, delimiter=",")
+                            writer.writerow(row)
