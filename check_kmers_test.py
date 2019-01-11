773f95717bedc7efa06322f3753b81ebb02a2067
hamazaki1990 <midsummerbeach@yahoo.co.jp>
2019-01-11 13:48:51 +0900

    multipul search

--- check_kmers_test.py
+++ check_kmers_test.py
@@ -1,56 +1,33 @@
 import csv
 from Bio import SeqIO
 from Bio import Seq
+from k_mers import collect_kmers
 
 
 chr = [str(x) for x in range(1, 23)]
 
 chr.extend(["X", "Y"])
 
-k = 10
+cent = [str(x) for x in range(1, 21)]
+
+cent.extend(["X", "Y"])
+
+cent.remove("5")
+cent.remove("19")
 
 
-def make_kmer(filename, formatname, k):
-    rec_iter = SeqIO.parse(filename, formatname)
-    i = 0
-    while True:
-        try:
-            seq_record = next(rec_iter)
-        except StopIteration:
-            break
-        else:
-            seq = seq_record.seq[i:i+k]
-            yield seq
-            i += 1
+input_q = "test1.fa"
+input_s = "test2.fa"
+
+k = 3
+
+kmer = collect_kmers(input_q, "fasta", k)
 
 
-for i in chr:
-    print(str(i))
-    input_q = "human_livingHORs.fa"
-    input_s = "chr" + str(i) + "_livingcentmask.fa"
-
-    kmer = make_kmer(input_q, "fasta", k)
-    output = "chr" + str(i) + "_" + str(k) + "mer_hit_test.csv"
-
-    for cent in SeqIO.parse(input_s, "fasta"):
-
-        with open(output, "w") as outfile:
-            while True:
-                try:
-                    seq = next(kmer)
-                    print(seq)
-                except:
-                    break
-                else:
-                    if len(seq) < k:
-                        break
-                    elif float(seq.count("N")) < 1:
-                        index = cent.seq.find(seq)
-                        if index == -1:
-                            pass
-                        else:
-                            row = [seq.id, index, str(seq), str(genome.seq.count(l)) + "times hit"]
-                            writer = csv.writer(outfile, delimiter=",")
-                            writer.writerow(row)
-                    else:
-                        pass
+for i in kmer:
+    for seq_record in SeqIO.parse(input_s, "fasta"):
+        index = seq_record.seq.find(i)
+        split = seq_record.seq.split(i)
+        length = [len(j) for j in split]
+        position = [sum(length[:(l + 1)]) + l * k for l in range(len(length) - 1)]
+        print(i, split, length, index, position)
