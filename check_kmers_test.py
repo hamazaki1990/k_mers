74d7f86bebdecd239ad5f1caea36839fa085ac84
hamazaki1990 <midsummerbeach@yahoo.co.jp>
2019-01-28 19:05:25 +0900

    kmer

--- check_kmers_test.py
+++ check_kmers_test.py
@@ -15,32 +15,32 @@ cent.extend(["X", "Y"])
 cent.remove("5")
 cent.remove("19")
 
+k = 20
 
-for k in range(15, 36):
-    for i in chr:
-        print("chr" + str(i))
-        input_s = "chr" + i + "_repeatmasked.fa"
-        try:
-            genome = next(SeqIO.parse(input_s, "fasta"))
-        except:
-            break
-        else:
-            for j in cent:
-                print(str(j))
-                input_q = "chr" + j + "_HOR.fa"
-                output = "chr" + i + "_" + str(k) + "mer_chr" + str(j) + "HOR_hit_test.csv"
-                kmer = collect_kmers(input_q, "fasta", k)
+for i in chr:
+    print("chr" + str(i))
+    input_s = "chr" + i + "_repeatmasked.fa"
+    try:
+        genome = next(SeqIO.parse(input_s, "fasta"))
+    except:
+        break
+    else:
+        for j in [1, 2, 11]:
+            print(str(j))
+            input_q = "chr" + j + "_HOR.fa"
+            output = "chr" + i + "_" + str(k) + "mer_chr" + str(j) + "HOR_hit_test.csv"
+            kmer = collect_kmers(input_q, "fasta", k)
 
-                with open(output, "w") as outfile:
-                    for l in kmer:
-                        index = genome.seq.find(l)
-                        if index == -1:
-                            pass
-                        else:
-                            split = genome.seq.split(l)
-                            length = [len(x) for x in split]
-                            position = [sum(length[:(x + 1)]) + x * k for x in range(len(length) - 1)]
-                            row = [l]
-                            row.extend(position)
-                            writer = csv.writer(outfile, delimiter=",")
-                            writer.writerow(row)
+            with open(output, "w") as outfile:
+                for l in kmer:
+                    index = genome.seq.find(l)
+                    if index == -1:
+                        pass
+                    else:
+                        split = genome.seq.split(l)
+                        length = [len(x) for x in split]
+                        position = [sum(length[:(x + 1)]) + x * k for x in range(len(length) - 1)]
+                        row = [l]
+                        row.extend(position)
+                        writer = csv.writer(outfile, delimiter=",")
+                        writer.writerow(row)
---
+++ kmer.r
@@ -0,0 +1,8 @@
+library(tidyverse)
+getwd()
+setwd("github/k_mers/")
+
+(ch<-read_csv("chrom_length.csv"))
+(ch<-ch%>%mutate("10mer"=ChromEnd/(4**10), "12mer"=ChromEnd/(4**12), "14mer"=ChromEnd/(4**14), "15mer"=ChromEnd/(4**15), "16mer"=ChromEnd/(4**16)))
+
+(hit<-read.csv("chr2_20mer_chr1HOR_hit_test.csv", col_names = c("cent", "kmer", "seq", "chr", "position")))
