a54ea623506f7aa26e2bbf9aa11ccdc265f82393
hamazaki1990 <midsummerbeach@yahoo.co.jp>
2019-05-20 08:59:43 +0900

    change

--- check_kmers.py
+++ check_kmers.py
@@ -1,8 +1,10 @@
 import csv
 from Bio import SeqIO
-from Bio import Seq
+from Bio.Seq import Seq
+from Bio.Alphabet import generic_dna
 from k_mers import collect_kmers
-
+import pandas as pd
+import numpy as np
 
 chr = [str(x) for x in range(1, 23)]
 
@@ -15,29 +17,32 @@ cent.extend(["X", "Y"])
 cent.remove("5")
 cent.remove("19")
 
-k = 20
 
 for i in chr:
     print("chr" + str(i))
     input_s = "chr" + i + "_livingcentmask.fa"
-    output = "chr" + i + "_" + str(k) + "mer_hit.gff"
     try:
         genome = next(SeqIO.parse(input_s, "fasta"))
     except:
         break
     else:
-        with open(output, "w") as outfile:
-            writer = csv.writer(outfile, delimiter=",")
-            writer.writerow(["##gff-version 3"])
-            for j in cent:
-                print(str(j))
-                input_q = "chr" + str(j) + "_centromere.fa"
-                kmer = collect_kmers(input_q, "fasta", k)
+        for j in cent:
+            print(str(j))
+            input_q = "centromere" + str(j) + "_original_kmer.csv"
+            output = "chr" + i + "_centromere" + str(j) + "kmer_hit.gff"
+            with open(output, "w") as outfile:
+                writer = csv.writer(outfile, delimiter=",")
+                writer.writerow(["##gff-version 3"])
+                df = pd.read_csv(input_q)
+                kmer = df.values.tolist()
+#                print(kmer)
 
                 for l in kmer:
-                    l_rc = l.reverse_complement()
-                    index = genome.seq.find(l)
-                    index_rc = genome.seq.find(l_rc)
+#                    print(l[0])
+                    seq = Seq(l[0], generic_dna)
+                    seq_rc = seq.reverse_complement()
+                    index = genome.seq.find(seq)
+                    index_rc = genome.seq.find(seq_rc)
                     if index == -1 and index_rc == -1:
                         pass
                     elif index_rc == -1:
@@ -58,7 +63,7 @@ for i in chr:
                         except:
                             break
                         else:
-                            split = genome.seq.split(l_rc)
+                            split = genome.seq.split(seq_rc)
                             length = [len(x) for x in split]
                             position = [sum(length[:(x + 1)]) + x * k for x in range(len(length) - 1)]
                             for m in position:
@@ -73,7 +78,7 @@ for i in chr:
                             split = genome.seq.split(l)
                             length = [len(x) for x in split]
                             position = [sum(length[:(x + 1)]) + x * k for x in range(len(length) - 1)]
-                            split_rc = genome.seq.split(l_rc)
+                            split_rc = genome.seq.split(seq_rc)
                             length_rc = [len(x) for x in split_rc]
                             position_rc = [sum(length_rc[:(x + 1)]) + x * k for x in range(len(length_rc) - 1)]
                             for m in position:
